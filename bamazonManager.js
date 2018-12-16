f17e75490e065e8329bd7c525b536c56aef5187f
Johnny Melo <Johnnymelowork@gmail.com>
2018-12-16 16:36:22 -0500

    bamazonManager now has a working function, and a list of commands that can be ran on the terminal

--- bamazonManager.js
+++ bamazonManager.js
@@ -4,8 +4,8 @@ const inquirer = require('inquirer');
 const Table = require("cli-table");
 
 // Block-scope the recordKeeping, databaseUpdate, and total
-let recordKeeping = [];
-let databaseUpdate = [];
+let thisDept = [];
+let deptArray = [];
 let total = 0;
 
 // Create a connection to mysql database
@@ -18,14 +18,242 @@ const connection = mysql.createConnection({
     database: "BamazonDB"
 });
 
-// List a set of menu options:
-function menuOptions(){
-connection.query("SELECT * FROM products", function (err, res)
-// View Products for Sale
+connection.connect(function(err) {
+    if (err) throw err;
 
-// View Low Inventory
+    console.log("");
+    console.log("Welcome Bamazon Manager!");
+    console.log("");
+    beginManager();
+});
 
-// Add to Inventory
+function beginManager() {
+    inquirer.prompt([
 
-// Add New Product
+        {
+            type: "list",
+            name: "option",
+            message: "please select an option.",
+            choices: ["View Products for Sale", "View Loe Inventory", "Add to Inventory", "Add New Product", "Exit"]
+        },
 
+    ]).then(function(manager) {
+
+        if (manager.option === "View Products for Sale") {
+
+            connection.query("SELECT * FROM products", function(err, res) {
+                if (err) throw err;
+
+                console.Table(res);
+
+                beginManager();
+
+            });
+
+
+
+
+        }
+        else if (manager.option === "View Low Inventory") {
+
+            connection.query("SELECT * FROM products WHERE stock_quality < 5", function(err, res) {
+                if (err) throw err;
+
+                if (res.length > 0) {
+
+                    console.Table(res);
+
+                }
+
+                else {
+
+                    console.log("");
+                    console.log("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+                    console.log("All items have 5 or more units currently.");
+                    console.log("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+                    console.log("");
+                }
+
+                beginManager();
+
+            });
+
+
+
+        }
+
+        else if (manager.option === "Add to Inventory") {
+
+            connection.query("SELECT * FROM products", function(err, res) {
+
+                if (err) throw err;
+
+                console.Table(res);
+
+
+                prompt.start();
+
+                prompt.get([
+
+                    {
+                        name: 'id',
+                        description: 'Please enter the TD of the item you would like to add inventory for',
+                        message: 'That is not a valid ID.',
+                        type: 'integer',
+                        required: true
+                    },
+
+                    {
+                        name: 'amount',
+                        description: 'Please enter the amount of units you are adding to this inventory',
+                        message: 'that is not a valid number.',
+                        type: 'integer',
+                        required: true
+                    },
+
+
+                ], function (err, result) {
+
+                    for (var i = 0; i < res.length; i++) {
+
+                        if (result.id === res[i].item_id){
+
+                            var newAmount = (res[i].stock_quantity + result.amount);
+                            var mySelection = res[i].product_name;
+
+                            connection.query("UPDATE products SET ? WHERE ?", [
+// This is updating the new stock amount
+
+                            {
+                                stock_quantity: newAmount 
+                            },
+
+// For the item that is the one that the users choose
+                            {
+                                item_id: result.id
+                            }
+                            ], function(err, res) {});
+
+                            console.log("")
+
+
+                            beginManager();
+
+
+
+                        }
+                    }
+                });
+
+            });
+
+
+
+        }
+
+        else if (manager.option === "Add New Product") {
+
+            connection.query("SELECT * FROM departments", function(err, res) {
+
+                if (err) throw err;
+
+                for (var i = 0; i < res.length; i++) {
+
+                    deptArray.push(res[i].department_name);
+                }
+
+
+
+
+
+                inquirier.prompt([
+
+                    {
+                        type: "list",
+                        name: "option",
+                        message: "Please select a department.",
+                        choices: deptArray
+                    },
+
+                ]).then(function(chooser) {
+
+                    thisDept = chooser.option;
+
+
+
+
+                    connection.query("SELECT * FROM products", function(err, res) {
+
+                        if (err) throw err;
+
+                        var myNextCount = (res.length + 1);
+
+
+                        prompt.start();
+
+                        prompt.get([
+
+                            {
+                                name: 'itemName',
+                                description: 'please enter the name of the product you would like to addto ' + thisDept,
+                                message: 'That is not a valid name.',
+                                type: 'string',
+                                required: true
+                            },
+
+                            {
+                                name: 'price',
+                                description: 'Please enter how much the item will cost',
+                                message: 'That is not a valid number.',
+                                type: 'number',
+                                required: true
+                            },
+
+
+                        ], function (err, result) {
+
+                            console.log("New item added to inventory!");
+
+
+
+
+
+                            connection.query("INSERT INTO products SET ?", {
+                                item_id: myNextCount,
+                                product_name: result.itemName,
+                                department_name: thisDept,
+                                price: result.price,
+                                stock_quantity: result.stock,
+                            }, function(err, res) {
+
+                                connection.query("SELECT * FROM products WHERE product_name = ?",
+                                result.itemName, function(err, reso) {
+                                    if (err) throw err;
+
+                                    console.Table(reso);
+                                    startManager();
+
+                                });
+
+
+                            });
+                        });
+
+                    });
+
+                });
+
+            });
+
+        }
+
+        else if (manager.option === "Exit") {
+
+            connection.end();
+        }
+
+    });
+
+
+
+}
